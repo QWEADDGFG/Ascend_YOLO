252871d65405dbdd18fa87aa8dc737137bd68dc7
QWEADDGFG <231060023@hdu.edu.cn>
2025-09-04 16:33:37 +0800

    update 20250904 16:33:37

--- git.sh
+++ git.sh
@@ -11,7 +11,7 @@ echo "[INFO] 清空缓存（unstage 所有已暂存的更改）..."
 git reset
 
 echo "[INFO] 添加所有更改（排除 Ascend_YOLO/output_* 文件夹）..."
-git add . ':!Ascend_YOLO/output_*'
+git add . ':!Ascend_YOLO/out2'
 
 echo "[INFO] 提交: $msg"
 git commit -m "$msg" || echo "[WARN] 没有更改需要提交"
